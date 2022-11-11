55a258fbcc272762d075162b2cad17ce12ca4265
Nathan Vonada <Nathan.Vonada@lakeshore.com>
2022-11-11 15:44:57 -0500

    Test project and unit test project

---
+++ Container.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Container.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lake Shore")]
+[assembly: AssemblyProduct("Container.Tests")]
+[assembly: AssemblyCopyright("Copyright © Lake Shore 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("724522ed-db2a-46ab-b5e0-05260f5b59c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DryIocTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
